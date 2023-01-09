716e99b34e761fa0c82fa5f099f4aaaba87a3d95
Muhammad Salman <muhammad.salman@keeptrucki.com>
2023-01-09 23:20:50 +0500

    Completed delete functionality

--- script.js
+++ script.js
@@ -6,16 +6,34 @@ const form = document.getElementById('form');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
-const expenses = [];
-const incomes = [];
+let expenses = [];
+let incomes = [];
 let totalExpense = 0, totalIncome = 0;
 
+function initialize() {
+  list.innerHTML = '';
+
+  expenses?.forEach(e => addTransaction(e));
+  incomes?.forEach(e => addTransaction(e));
+}
+
 function addArrayElements(array) {
-  return array.reduce((partialSum, acc) => partialSum + acc, 0);
+  return array.reduce((partialSum, acc) => partialSum + acc.amount, 0);
 }
 
 function calculateBalance(totalIncome, totalExpense) {
   balance.innerText = `$${totalIncome + totalExpense}`;
+}
+
+function deleteTransaction(id, isExpense) {
+  console.log(id);
+  if (isExpense) {
+    expenses = expenses.filter(expense => expense.id !== id);
+  } else {
+    incomes = incomes.filter(income => income.id !== id);
+  }
+
+  initialize();
 }
 
 function addTransaction(e) {
@@ -32,29 +50,38 @@ function addTransaction(e) {
   }
 
   if (amount.value[0] === '-') {
-    list.innerHTML += `
-      <li class="minus">
-        ${text.value} <span>${amount.value}</span><button class="delete-btn">x</button>
-      </li>
-    `;
+    const expense = {
+      amount: Number(amount.value),
+      id: Math.floor(Math.random() * 10000000),
+    };
 
-    expenses.push(Number(amount.value));
+    expenses.push(expense);
     totalExpense = addArrayElements(expenses);
     money_minus.innerText = `$${totalExpense * -1}`;
-  } else {
+
     list.innerHTML += `
-      <li class="plus">
-        ${text.value} <span>+${amount.value}</span><button class="delete-btn">x</button>
+      <li class="minus">
+        ${text.value} <span>${amount.value}</span><button class="delete-btn" onclick="deleteTransaction(${expense.id}, true)">x</button>
       </li>
     `;
+  } else {
+    const income = {
+      amount: Number(amount.value),
+      id: Math.floor(Math.random() * 10000000),
+    };
 
-    incomes.push(Number(amount.value));
+    incomes.push(income);
     totalIncome = addArrayElements(incomes);
     money_plus.innerText = `$${totalIncome}`;
+
+    list.innerHTML += `
+      <li class="plus">
+        ${text.value} <span>+${amount.value}</span><button class="delete-btn" onclick="deleteTransaction(${income.id}, false)">x</button>
+      </li>
+    `;
   }
 
   calculateBalance(totalIncome, totalExpense);
-
 }
 
 form.addEventListener('submit', addTransaction);
